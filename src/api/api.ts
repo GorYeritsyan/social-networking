e3e89e339143770d98eda4b65c87cbc998ef56b2
GorYeritsyan <gor.k.yeritsyan@gmail.com>
2024-09-09 01:30:16 +0400

    env

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,8 +5,8 @@ import { EditProfileDataType, LoginType } from "../types/types";
 export const imageUrl = "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80";
 
 //Authorization
-const API_KEY = process.env.REACT_APP_API_KEY;
-export const token = process.env.REACT_APP_BEAR_TOKEN;
+const API_KEY = 'd6ad6035-023d-4093-b438-0b99459c3256';
+export const token = '73638a62-14e5-4390-a38c-720a73a6d32f';
 
 const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.0",
